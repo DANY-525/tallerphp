1cfebd6f86178a27e9b8246727cfdfebbbf38ada
daniel <dany525@gmail.com>
2018-09-07 18:04:41 -0500

    listado de usuarios bootstraps

--- view/usuarios/listarUsuario.php
+++ view/usuarios/listarUsuario.php
@@ -13,7 +13,7 @@
 
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Listado Usuarios</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -21,7 +21,7 @@
   <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Agregar<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php?modulo=usuarios&funcion=create">Agregar<span class="sr-only">(current)</span></a>
       </li>
       
     </ul>
